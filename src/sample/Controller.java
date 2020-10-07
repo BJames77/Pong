fc59ba2f4db953ba55014e21e8356059fdb728fb
t-gott <70244874+t-gott@users.noreply.github.com>
2020-10-07 16:43:04 -0500

    Added comments to the controller class

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -11,26 +11,26 @@ public class Controller {
     @FXML private Rectangle p1paddle;
     @FXML private Rectangle p2paddle;
 
-    @FXML
+    //method called by the AnchorPane and is accessed on every keypress
     public void paddleMove(KeyEvent event) {
-        double heightP1 = p1paddle.getY();
-        double heightP2 = p2paddle.getY();
+        double heightP1 = p1paddle.getY(); //sets a local height variable for p1 equivalent to p1paddle's Y location
+        double heightP2 = p2paddle.getY(); //sets a local height variable for p2 equivalent to p2paddle's Y location
 
+        //switch that takes the keyboard input and changes the outcome according to which key was pressed
         switch (event.getCode()) {
             case W:
-                p1paddle.setY(heightP1-15);
+                p1paddle.setY(heightP1-15); //updates the Y coordinate of paddle 1 up 15 pixels
                 break;
             case S:
-                p1paddle.setY(heightP1+15);
+                p1paddle.setY(heightP1+15); //updates the Y coordinate of paddle 1 down 15 pixels
                 break;
             case UP:
-                p2paddle.setY(heightP2-15);
+                p2paddle.setY(heightP2-15); //updates the Y coordinate of paddle 2 up 15 pixels
                 break;
             case DOWN:
-                p2paddle.setY(heightP2+15);
+                p2paddle.setY(heightP2+15); //updates the Y coordinate of paddle 2 down 15 pixels
                 break;
             default:
-                System.out.println("Invalid key");
                 break;
         }
 
